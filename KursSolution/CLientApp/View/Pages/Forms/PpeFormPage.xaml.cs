67da10b5c6382ccb516c31802a74bc157d9cdf38
Denoos <148026464+Denoos@users.noreply.github.com>
2025-05-14 23:11:29 +1000

    ВСЁ ГОТОВО КРОМЕ УДАЛЕНИЯ

--- KursSolution/CLientApp/View/Pages/Forms/PpeFormPage.xaml.cs
+++ KursSolution/CLientApp/View/Pages/Forms/PpeFormPage.xaml.cs
@@ -37,8 +37,8 @@ namespace CLientApp.View.Pages.Forms
         public event PropertyChangedEventHandler? PropertyChanged;
 
         public Ppe Item { get => item; set { item = value; Signal(); } }
-        private List<Model.Condition> Items { get => roles; set { roles = value; Signal(); } }
-        private List<PpeType> ItemsSecond { get => itemsSecond; set { itemsSecond = value; Signal(); } }
+        public List<Model.Condition> Items { get => roles; set { roles = value; Signal(); } }
+        public List<PpeType> ItemsSecond { get => itemsSecond; set { itemsSecond = value; Signal(); } }
         public bool IsEnabled { get => isEnabled; set { isEnabled = value; Signal(); } }
 
         private void Signal([CallerMemberName] string? prop = null)
@@ -49,7 +49,7 @@ namespace CLientApp.View.Pages.Forms
             InitializeComponent();
             _mainWindow = window;
             IsEnabled = IsEn;
-            _ = GetItems();
+            GetItems();
             if (item is not null)
             {
                 isAdd = false;
@@ -57,12 +57,22 @@ namespace CLientApp.View.Pages.Forms
             }
             else Item = new();
             DataContext = this;
+
+            if (Item.Condition is not null)
+                Conditionus.SelectedValue = Item.Condition;
+            else Item.Condition = new Model.Condition() { Title = "Не выбрано!" };
+
+            if (Item.Type is not null)
+                Typus.SelectedValue = Item.Type;
+            else Item.Type = new PpeType() { Title = "Не выбрано!" };
+
+            Signal();
         }
 
         private async Task GetItems()
         {
-            Items = await _db.GetAllConditions();
-            ItemsSecond = await _db.GetAllPpeTypes();
+            Items = [.. await _db.GetAllConditions()];
+            ItemsSecond = [.. await _db.GetAllPpeTypes()];
         }
 
         private async void Save_Click(object sender, RoutedEventArgs e)
@@ -92,5 +102,19 @@ namespace CLientApp.View.Pages.Forms
 
         private void Exit_Click(object sender, RoutedEventArgs e)
             => _mainWindow.SetPage(new PpePageMenu(_mainWindow));
+
+        private void GetDateChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var snd = (DatePicker)sender;
+            Item.DateGet = DateOnly.FromDateTime((DateTime)snd.SelectedDate);
+            getDate.Text = Item.DateGet.ToString(); 
+        }
+        
+        private void EndDateChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var snd = (DatePicker)sender;
+            Item.DateEnd= DateOnly.FromDateTime((DateTime)snd.SelectedDate);
+            endDate.Text = Item.DateEnd.ToString();
+        }
     }
 }
